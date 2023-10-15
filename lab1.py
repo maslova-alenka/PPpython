d108827a01f3adee67da258a2acf2c40a8cb0848
Светлана Маслова <svetlana111019@outlook.com>
2023-10-15 13:57:53 +0400

    добавлены функции

--- lab1.py
+++ lab1.py
@@ -1,3 +1,28 @@
 import requests
+from bs4 import BeautifulSoup
+import os
+
 URL="https://yandex.ru/images/"
- 
+headers={"User-Agent": "Mozilla/5.0"}
+
+def download_image(path, key):
+    response=requests.get(URL,headers)
+    
+    soup=BeautifulSoup(response.text, "lxml")
+    images=soup.findAll('img', class_='serp-item__thumb justifier__thumb')
+    if not images:
+        print("No images found")
+        
+
+    
+    
+    
+    
+   
+   
+def main():
+    directory=os.getcwd()
+     
+    
+if __name__=="__main__":
+    main()
